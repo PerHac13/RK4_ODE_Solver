6e70e6dbc4501217237b6c332c61034cee2ef2b2
PerHac13 <abdullah13112004@gmail.com>
2024-10-28 19:09:19 +0530

    chore!: equation headers file

---
+++ include/equation.h
@@ -0,0 +1,7 @@
+
+// Equations are dy/dx
+class Equation{
+    public:
+        static double differentialEquation1(double x, double y);
+        static double differentialEquation2(double x, double y);
+};
