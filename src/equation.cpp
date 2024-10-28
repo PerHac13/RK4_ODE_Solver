90230f2c2dfa93e154d973453eb05288f671a4f8
PerHac13 <abdullah13112004@gmail.com>
2024-10-28 19:11:23 +0530

    feat!: equation.cpp

---
+++ src/equation.cpp
@@ -0,0 +1,10 @@
+#include "equation.h"
+#include <cmath>
+
+double Equation::differentialEquation1(double x, double y){
+    return y+x+3;
+}
+
+double Equation::differentialEquation2(double x, double y){
+    return 2*y-x*x+1;
+}
